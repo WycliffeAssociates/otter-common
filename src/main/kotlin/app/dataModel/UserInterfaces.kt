8caf6b0c0c19b75eff9b57495ed99520a87602e0
Acliu1498 <acliu1498@gmail.com>
2018-07-10 15:46:18 -0400

    added a userModel

--- src/main/kotlin/app/dataModel/UserInterfaces.kt
+++ src/main/kotlin/app/dataModel/UserInterfaces.kt
@@ -1,17 +1,10 @@
 package app.dataModel
 
-import io.requery.Entity
-import io.requery.Generated
-import io.requery.Key
-import io.requery.Persistable
-
-@Entity
-interface User: Persistable{
-
-    @get:Key
-    @get:Generated
-    var _id: Int
-    var hash: String
+data class UserModel constructor(
+    var _id: Int,
+    var hash: String,
     var recordedNamePath: String
-}
+)
+
+
 
