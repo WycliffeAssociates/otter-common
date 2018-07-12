04024fe16d59de74c34f86cdbef02167d70d350c
Acliu1498 <acliu1498@gmail.com>
2018-07-12 16:06:07 -0400

    Added

--- src/main/kotlin/data/User.kt
+++ src/main/kotlin/data/User.kt
@@ -5,7 +5,7 @@ data class User(
         var audioHash: String,
         val audioPath: String,
         val sourceLanguages: MutableList<Language>,
-        val targetLanguage: MutableList<Language>,
+        val targetLanguages: MutableList<Language>,
         var preferredSourceLanguage: Language,
         var preferredTargetLanguage: Language
 )
