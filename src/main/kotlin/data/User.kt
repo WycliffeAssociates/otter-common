c07d44590558f21ff6c077bf5a62f27c0bcf7853
Acliu1498 <acliu1498@gmail.com>
2018-07-12 15:14:47 -0400

    users

--- src/main/kotlin/data/User.kt
+++ src/main/kotlin/data/User.kt
@@ -4,8 +4,8 @@ data class User(
         var id: Int = 0,
         var audioHash: String,
         val audioPath: String,
-        var sourceLanguages: List<Language>,
-        var targetLanguage: List<Language>,
+        val sourceLanguages: List<Language>,
+        val targetLanguage: List<Language>,
         var preferredSourceLanguage: Language,
         var preferredTargetLanguage: Language
 )
