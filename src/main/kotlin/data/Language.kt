30603fcb8f37798730f964e18d02bb6777d7ae2b
Acliu1498 <acliu1498@gmail.com>
2018-07-16 09:51:31 -0400

    Added language attributes

--- src/main/kotlin/data/Language.kt
+++ src/main/kotlin/data/Language.kt
@@ -3,5 +3,7 @@ package data
 data class Language(
         var id: Int = 0,
         val slug: String,
-        val name: String
+        val name: String,
+        val type: String,
+        val anglicizedName: String
 )
