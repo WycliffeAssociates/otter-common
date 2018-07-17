de5b49a8761f9034a29a435f2a9834bf120ecb31
Acliu1498 <acliu1498@gmail.com>
2018-07-17 15:31:48 -0400

    trdb update

--- src/main/kotlin/data/persistence/TrDatabase.kt
+++ src/main/kotlin/data/persistence/TrDatabase.kt
@@ -1,8 +1,10 @@
 package data.persistence
 
+import data.Language
 import data.User
 import data.dao.Dao
 
 interface TrDatabase {
      fun getUserDao(): Dao<User>
+     fun geLanguageDao(): Dao<Language>
 }
