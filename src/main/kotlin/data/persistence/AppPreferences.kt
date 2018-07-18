1640bf418700e84d56d7a53d904164102af4a4f1
Matthew Russell <mbr4477@gmail.com>
2018-07-18 09:20:19 -0400

    Changed setCurrentUserHash to putCurrentUserHash to match preferences API nomenclature

--- src/main/kotlin/data/persistence/AppPreferences.kt
+++ src/main/kotlin/data/persistence/AppPreferences.kt
@@ -3,5 +3,5 @@ package data.persistence
 // interface to getting user-independent app preferences
 interface AppPreferences {
     fun getCurrentUserHash() : String // should return an empty string if no current user hash
-    fun setCurrentUserHash(userHash: String)
+    fun putCurrentUserHash(userHash: String)
 }
