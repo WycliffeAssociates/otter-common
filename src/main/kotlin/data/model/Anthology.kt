5b77ec36823e0ce5361359f1ca7dc8b63079c3c4
Acliu1498 <acliu1498@gmail.com>
2018-07-23 11:29:08 -0400

    changes

--- src/main/kotlin/data/model/Anthology.kt
+++ src/main/kotlin/data/model/Anthology.kt
@@ -2,7 +2,7 @@ package data.model
 
 data class Anthology(
         var id: Int = 0,
-        val title: String,
-        val sort: Int,
-        val Languages: MutableSet<Language>
+        val title: String, // This will be a key
+        val label: String,
+        val sort: Int
 )
