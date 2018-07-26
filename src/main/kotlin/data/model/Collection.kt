2f6eb6e72e997ca032715b6de82dea424a4b2510
Matthew Russell <mbr4477@gmail.com>
2018-07-26 16:14:50 -0400

    Updated Collection class

--- src/main/kotlin/data/model/Collection.kt
+++ src/main/kotlin/data/model/Collection.kt
@@ -3,8 +3,6 @@ package data.model
 open class Collection (
         val id: Int = 0,
         val label: String,
-        val belongsTo: String,
         val titleKey: String,
-        val sort: Int,
-        val imagePath: String
+        val sort: Int
 )
