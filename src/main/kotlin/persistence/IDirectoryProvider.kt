cec3106e19e603f3e4f560b4e9d04ba0d2ef32cb
Matthew Russell <mbr4477@gmail.com>
2018-08-02 15:51:39 -0400

    Changed return type to File; moved to persistence package

--- src/main/kotlin/persistence/IDirectoryProvider.kt
+++ src/main/kotlin/persistence/IDirectoryProvider.kt
@@ -1,10 +1,12 @@
-package filesystem
+package persistence
+
+import java.io.File
 
 interface IDirectoryProvider {
 
     // create a directory to store the user's application projects/documents
-    fun getUserDataDirectory(appendedPath: String = "") : String
+    fun getUserDataDirectory(appendedPath: String = "") : File
 
     // create a directory to store the application's private data
-    fun getAppDataDirectory(appendedPath: String = "") : String
+    fun getAppDataDirectory(appendedPath: String = "") : File
 }
