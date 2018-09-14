308dede23b6fcaaed68f6ad7c25bcc1b0aa8ac1e
Matthew Russell <mbr4477@gmail.com>
2018-09-14 15:45:12 -0400

    Add ResourceContainer and fix Take

---
+++ src/main/kotlin/org/wycliffeassociates/otter/common/data/model/ResourceContainer.kt
@@ -0,0 +1,22 @@
+package org.wycliffeassociates.otter.common.data.model
+
+import java.io.File
+import java.util.*
+
+data class ResourceContainer(
+        var id: Int,
+        var conformsTo: String,
+        var creator: String,
+        var description: String,
+        var format: String,
+        var identifier: String,
+        var issued: Calendar,
+        var language: Language,
+        var modified: Calendar,
+        var publisher: String,
+        var subject: String,
+        var type: String,
+        var title: String,
+        var version: Int,
+        var path: File
+)
--- src/main/kotlin/org/wycliffeassociates/otter/common/data/model/Take.kt
+++ src/main/kotlin/org/wycliffeassociates/otter/common/data/model/Take.kt
@@ -1,14 +1,13 @@
 package org.wycliffeassociates.otter.common.data.model
 
 import java.io.File
-import java.util.Date
+import java.util.*
 
 data class Take(
         var id: Int = 0,
         var filename: String,
         var path: File,
         var number: Int,
-        var timestamp: Date,
-        var filePath: String,
+        var timestamp: Calendar,
         var isUnheard: Boolean
 )
