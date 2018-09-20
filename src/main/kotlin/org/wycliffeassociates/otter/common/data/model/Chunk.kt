24fc03a75d31730e149e1df987990845f33f3fc8
Matthew Russell <mbr4477@gmail.com>
2018-09-20 12:08:41 -0400

    Changed val to var in chunk

--- src/main/kotlin/org/wycliffeassociates/otter/common/data/model/Chunk.kt
+++ src/main/kotlin/org/wycliffeassociates/otter/common/data/model/Chunk.kt
@@ -3,8 +3,8 @@ package org.wycliffeassociates.otter.common.data.model
 data class Chunk(
         val sort: Int,
         var labelKey: String,
-        val start: Int,
-        val end: Int,
+        var start: Int,
+        var end: Int,
         var selectedTake: Take?,
         var id: Int = 0
 )
