3f0b844df4b8f97cd9c1ae3aa738392b05ddd113
Matthew Russell <mbr4477@gmail.com>
2018-09-20 12:09:42 -0400

    Change val to var in chunk (sort)

--- src/main/kotlin/org/wycliffeassociates/otter/common/data/model/Chunk.kt
+++ src/main/kotlin/org/wycliffeassociates/otter/common/data/model/Chunk.kt
@@ -1,7 +1,7 @@
 package org.wycliffeassociates.otter.common.data.model
 
 data class Chunk(
-        val sort: Int,
+        var sort: Int,
         var labelKey: String,
         var start: Int,
         var end: Int,
