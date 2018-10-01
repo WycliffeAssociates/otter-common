0c8dbb0135570b1dbf15fddb02648da3c5695573
Matthew Russell <mbr4477@gmail.com>
2018-10-01 11:42:10 -0400

    Repo interface updates

--- src/main/kotlin/org/wycliffeassociates/otter/common/persistence/repositories/IResourceMetadataRepository.kt
+++ src/main/kotlin/org/wycliffeassociates/otter/common/persistence/repositories/IResourceMetadataRepository.kt
@@ -1,5 +1,8 @@
 package org.wycliffeassociates.otter.common.persistence.repositories
 
+import io.reactivex.Single
 import org.wycliffeassociates.otter.common.data.model.ResourceMetadata
 
-interface IResourceMetadataRepository : IRepository<ResourceMetadata>
+interface IResourceMetadataRepository : IRepository<ResourceMetadata> {
+    fun insert(metadata: ResourceMetadata): Single<Int>
+}
--- src/main/kotlin/org/wycliffeassociates/otter/common/persistence/repositories/ISourceRepository.kt
+++ src/main/kotlin/org/wycliffeassociates/otter/common/persistence/repositories/ISourceRepository.kt
@@ -6,6 +6,7 @@ import org.wycliffeassociates.otter.common.data.model.ProjectCollection
 import org.wycliffeassociates.otter.common.data.model.SourceCollection
 
 interface ISourceRepository : IRepository<SourceCollection> {
+    fun insert(source: SourceCollection): Single<Int>
     fun getAllRoot(): Single<List<SourceCollection>>
     fun getByProjectCollection(project: ProjectCollection): Single<SourceCollection>
     fun getChildren(source: SourceCollection): Single<List<SourceCollection>>
