1431e25cc37a2ed1a3040b07020ee0ca15a8bb43
Matthew Russell <mbr4477@gmail.com>
2018-10-30 15:13:39 -0400

    Support separate, persistent editor and recorder selections

--- src/main/kotlin/org/wycliffeassociates/otter/common/domain/plugins/InitializePlugins.kt
+++ src/main/kotlin/org/wycliffeassociates/otter/common/domain/plugins/InitializePlugins.kt
@@ -1,14 +1,12 @@
 package org.wycliffeassociates.otter.common.domain.plugins
 
 import io.reactivex.Completable
+import io.reactivex.Maybe
+import io.reactivex.Single
 import org.wycliffeassociates.otter.common.persistence.repositories.IAudioPluginRepository
 
 class InitializePlugins(
         private val pluginRepository: IAudioPluginRepository
 ) {
-    fun initDefault(): Completable = pluginRepository
-                .getAll()
-                .flatMapCompletable {
-                    pluginRepository.setDefaultPluginData(it.firstOrNull())
-                }
+    fun init(): Completable = pluginRepository.initSelected()
 }
