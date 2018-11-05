042f48080764962f0fcbb292f23886fd3c7476f2
Joe <joe_sarabia@wycliffeassociates.org>
2018-11-05 11:35:29 -0500

    Fast import working

--- src/main/kotlin/org/wycliffeassociates/otter/common/persistence/repositories/ICollectionRepository.kt
+++ src/main/kotlin/org/wycliffeassociates/otter/common/persistence/repositories/ICollectionRepository.kt
@@ -6,6 +6,7 @@ import io.reactivex.Single
 import org.wycliffeassociates.otter.common.collections.tree.Tree
 import org.wycliffeassociates.otter.common.data.model.Collection
 import org.wycliffeassociates.otter.common.data.model.ResourceMetadata
+import org.wycliffeassociates.resourcecontainer.ResourceContainer
 
 interface ICollectionRepository : IRepository<Collection> {
     fun insert(collection: Collection): Single<Int>
@@ -13,5 +14,5 @@ interface ICollectionRepository : IRepository<Collection> {
     fun getChildren(collection: Collection): Single<List<Collection>>
     fun updateSource(collection: Collection, newSource: Collection): Completable
     fun updateParent(collection: Collection, newParent: Collection): Completable
-    fun importResourceContainer(tree: Tree, languageSlug: String): Completable
+    fun importResourceContainer(rc: ResourceContainer, tree: Tree, languageSlug: String): Completable
 }
