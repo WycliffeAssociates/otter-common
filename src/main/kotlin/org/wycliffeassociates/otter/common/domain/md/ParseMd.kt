d46c1b4da943df9ea1117bc843f370bbb994f32f
KJoslyn <KJoslyn29@gmail.com>
2019-01-31 14:57:24 -0500

    Made ParseMd an object

--- src/main/kotlin/org/wycliffeassociates/otter/common/domain/md/ParseMd.kt
+++ src/main/kotlin/org/wycliffeassociates/otter/common/domain/md/ParseMd.kt
@@ -5,41 +5,39 @@ import java.io.BufferedReader
 // TODO: Add Help type enum to HelpResource? (tn, tq)
 data class HelpResource(var title: String, var body: String)
 
-class ParseMd {
-    companion object {
+object ParseMd {
 
-        private val isTitleRegex = Regex("^#+\\s*[^#\\s]+")
-        private val titleTextRegex = Regex("^#+\\s*")
+    private val isTitleRegex = Regex("^#+\\s*[^#\\s]+")
+    private val titleTextRegex = Regex("^#+\\s*")
 
-        fun parse(reader: BufferedReader): List<HelpResource> {
-            val helpResourceList = ArrayList<HelpResource>()
+    fun parse(reader: BufferedReader): List<HelpResource> {
+        val helpResourceList = ArrayList<HelpResource>()
 
-            reader.forEachLine {
-                if (it.isEmpty()) {
-                    return@forEachLine // continue
-                }
-                // If we have a title, add a new help resource to the end of the list
-                if (isTitleLine(it)) {
-                    helpResourceList.add(HelpResource(it, ""))
-                }
-                // Found body text. Add the body to the help resource at the end of the list
-                // If the list is empty, the body text will be discarded (this should never happen.)
-                else if (helpResourceList.isNotEmpty()) {
-                    if (helpResourceList.last().body.isNotEmpty()) {
-                        helpResourceList.last().body += System.lineSeparator()
-                    }
-                    helpResourceList.last().body += it
-                }
+        reader.forEachLine {
+            if (it.isEmpty()) {
+                return@forEachLine // continue
             }
-            return helpResourceList
+            // If we have a title, add a new help resource to the end of the list
+            if (isTitleLine(it)) {
+                helpResourceList.add(HelpResource(it, ""))
+            }
+            // Found body text. Add the body to the help resource at the end of the list
+            // If the list is empty, the body text will be discarded (this should never happen.)
+            else if (helpResourceList.isNotEmpty()) {
+                if (helpResourceList.last().body.isNotEmpty()) {
+                    helpResourceList.last().body += System.lineSeparator()
+                }
+                helpResourceList.last().body += it
+            }
         }
-
-        internal fun getTitleText(line: String): String? {
-            titleTextRegex.find(line)?.let {
-                return line.removePrefix(it.value)
-            } ?: return null
-        }
-
-        internal fun isTitleLine(line: String): Boolean = isTitleRegex.containsMatchIn(line)
+        return helpResourceList
     }
+
+    internal fun getTitleText(line: String): String? {
+        titleTextRegex.find(line)?.let {
+            return line.removePrefix(it.value)
+        } ?: return null
+    }
+
+    internal fun isTitleLine(line: String): Boolean = isTitleRegex.containsMatchIn(line)
 }
