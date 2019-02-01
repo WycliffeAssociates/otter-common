caf78ed43a81e46476d085dbb8cc56232269ddc3
Russell Aunger <rba@live.com>
2019-02-01 18:42:15 -0500

    Implement MarkdownProjectReader.

--- src/main/kotlin/org/wycliffeassociates/otter/common/domain/resourcecontainer/project/markdown/MarkdownProjectReader.kt
+++ src/main/kotlin/org/wycliffeassociates/otter/common/domain/resourcecontainer/project/markdown/MarkdownProjectReader.kt
@@ -1,13 +1,104 @@
 package org.wycliffeassociates.otter.common.domain.resourcecontainer.project.markdown
 
+import org.wycliffeassociates.otter.common.collections.tree.OtterTree
+import org.wycliffeassociates.otter.common.collections.tree.OtterTreeNode
 import org.wycliffeassociates.otter.common.collections.tree.Tree
+import org.wycliffeassociates.otter.common.collections.tree.TreeNode
+import org.wycliffeassociates.otter.common.data.model.Collection
+import org.wycliffeassociates.otter.common.data.model.Content
 import org.wycliffeassociates.otter.common.domain.resourcecontainer.ImportResult
 import org.wycliffeassociates.otter.common.domain.resourcecontainer.project.IProjectReader
 import org.wycliffeassociates.resourcecontainer.ResourceContainer
 import org.wycliffeassociates.resourcecontainer.entity.Project
+import java.io.BufferedReader
+import java.io.File
+import java.util.*
+
+private const val FORMAT = "text/markdown"
+private val extensions = Regex(".+\\.(md|mkdn?|mdown|markdown)$", RegexOption.IGNORE_CASE)
 
 class MarkdownProjectReader: IProjectReader {
-    override fun constructProjectTree(container: ResourceContainer, project: Project): Pair<ImportResult, Tree> {
-        TODO("The MarkdownProjectReader isn't yet implemented.")
+    override fun constructProjectTree(container: ResourceContainer, project: Project)
+            : Pair<ImportResult, Tree> {
+        val projectRoot = container.dir.resolve(project.path)
+        return projectRoot
+                .buildFileTree()
+                .filterMarkdownFiles()
+                ?.map<Any> { f -> contentList(f) ?: collection(f, projectRoot) }
+                ?.flattenContent()
+                ?.let { Pair(ImportResult.SUCCESS, it) }
+                ?: Pair(ImportResult.LOAD_RC_ERROR, Tree(Unit))
+    }
+
+    private fun fileToId(f: File): Int =
+        f.nameWithoutExtension.toIntOrNull() ?: 0
+
+    private fun fileToSlug(f: File, root: File): String =
+        f.toRelativeString(root.parentFile)
+                .split('/', '\\')
+                .map { it.toIntOrNull()?.toString() ?: it }
+                .joinToString("_")
+
+    private fun bufferedReaderProvider(f: File): (() -> BufferedReader)? =
+            if (f.isFile) { { f.bufferedReader() } } else null
+
+    private fun collection(f: File, projectRoot: File): Collection =
+            collection(fileToSlug(f, projectRoot), fileToId(f))
+
+    private fun collection(slug: String, id: Int): Collection =
+            Collection(
+                    sort = id,
+                    slug = slug,
+                    labelKey = "helpcollection",
+                    titleKey = "$id",
+                    resourceContainer = null)
+
+    private fun contentList(f: File): List<Content>? =
+            bufferedReaderProvider(f)
+                    ?.let { contentList(it, fileToId(f)) }
+
+    private fun contentList(brp: () -> BufferedReader, fileId: Int): List<Content> {
+        val helpResources = brp().use { ParseMd.parse(it) }
+        var sort = 1
+        return helpResources.flatMap { helpResource -> listOf(
+                    Content(sort++, "title", fileId, fileId, null, helpResource.title, FORMAT),
+                    Content(sort++, "body", fileId, fileId, null, helpResource.body, FORMAT)
+            )}
     }
 }
+
+internal fun File.buildFileTree(): OtterTree<File> {
+    var treeRoot: OtterTree<File>? = null
+    val treeCursor = ArrayDeque<OtterTree<File>>()
+    this.walkTopDown()
+            .onEnter { newDir ->
+                OtterTree(newDir).let { newDirNode ->
+                    treeCursor.peek()?.addChild(newDirNode)
+                    treeCursor.push(newDirNode)
+                    true
+                }
+            }
+            .onLeave { treeRoot = treeCursor.pop() }
+            .filter { it.isFile }
+            .map { OtterTreeNode(it) }
+            .forEach { treeCursor.peek()?.addChild(it) }
+    return treeRoot ?: OtterTree(this)
+}
+
+private fun OtterTree<File>.filterMarkdownFiles(): OtterTree<File>? =
+        this.filterPreserveParents { it.isFile && extensions.matches(it.name) }
+
+private fun Tree.flattenContent(): Tree =
+        Tree(this.value).also {
+            it.addAll(
+                    if (this.children.all { c -> c.value is List<*> }) {
+                        this.children
+                                .flatMap { c -> c.value as List<*> }
+                                .filterNotNull()
+                                .map { TreeNode(it) }
+                    } else {
+                        this.children
+                                .map { if (it is Tree) it.flattenContent() else it }
+                    }
+            )
+        }
