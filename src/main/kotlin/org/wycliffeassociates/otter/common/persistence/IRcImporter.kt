4c253a9ccb2befe723d1e992bb2ac108eeb36e43
Kevin Joslyn <joslynk@wa.wycliffe.org>
2019-02-06 00:31:27 -0500

    Refactoring import resource container code

---
+++ src/main/kotlin/org/wycliffeassociates/otter/common/persistence/IRcImporter.kt
@@ -0,0 +1,9 @@
+package org.wycliffeassociates.otter.common.persistence
+
+import io.reactivex.Completable
+import org.wycliffeassociates.otter.common.collections.tree.Tree
+import org.wycliffeassociates.resourcecontainer.ResourceContainer
+
+interface IRcImporter {
+    fun importResourceContainer(rc: ResourceContainer, rcTree: Tree, languageSlug: String): Completable
+}
