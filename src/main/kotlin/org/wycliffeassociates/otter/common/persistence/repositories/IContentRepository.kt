74a6025f6d73ae56c2668830ac1ebd366984a882
Kevin Joslyn <joslynk@wa.wycliffe.org>
2019-02-06 02:48:29 -0500

    Added function to support creating Resource entries

--- src/main/kotlin/org/wycliffeassociates/otter/common/persistence/repositories/IContentRepository.kt
+++ src/main/kotlin/org/wycliffeassociates/otter/common/persistence/repositories/IContentRepository.kt
@@ -1,6 +1,7 @@
 package org.wycliffeassociates.otter.common.persistence.repositories
 
 import io.reactivex.Completable
+import io.reactivex.Maybe
 import io.reactivex.Single
 import org.wycliffeassociates.otter.common.data.model.Content
 import org.wycliffeassociates.otter.common.data.model.Collection
@@ -10,6 +11,8 @@ interface IContentRepository : IRepository<Content> {
     fun insertForCollection(content: Content, collection: Collection): Single<Int>
     // Get all the chunks for a collection
     fun getByCollection(collection: Collection): Single<List<Content>>
+    // Get the chunk in a collection by the start field
+    fun getByCollectionAndStart(collection: Collection, start: Int): Maybe<Content>
     // Get sources this content is derived from
     fun getSources(content: Content): Single<List<Content>>
     // Update the sources for a content
