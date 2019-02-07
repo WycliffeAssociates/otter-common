Bible-Translation-Tools/Orature

commit 266fb417fb006ebbcddf58bd563dd24965ad69f9

Select Markdown importer path based on RC format (#82)
* Minor refactoring, UsfmParser

* Refactor out UsfmProjectReader.

* Move code into domain/resourcecontainer/project.

* IProjectReader factory.

* Promote ImportResult enum from ImportResourceContainer.

src/main/kotlin/org/wycliffeassociates/otter/common/domain/resourcecontainer/project/usfm/ParseUsfm.kt
@@ -1,4 +1,4 @@
-package org.wycliffeassociates.otter.common.domain.usfm
+package org.wycliffeassociates.otter.common.domain.resourcecontainer.project.usfm
 
 import java.io.File
 
@@ -22,7 +22,6 @@ data class Verse(val number: Int, var text: String)
 class UsfmDocument : HashMap<Int, HashMap<Int, Verse>>()
 
 class ParseUsfm(val file: File) {
-    val current = Current()
     val chapters: UsfmDocument = UsfmDocument()
 
     fun parse(): ParseUsfm {
@@ -40,12 +39,13 @@ class ParseUsfm(val file: File) {
         if (split.isEmpty()) {
             return
         }
+        val current = Current()
         when (split[0]) {
             MARKER_BOOK_NAME -> return
             MARKER_CHAPTER_NUMBER -> {
-                current.c = split[1]?.let {
-                    it.replace("\\s".toRegex(), "").toInt() //strip potential whitespace and convert to int
-                }
+                current.c = split[1]
+                        ?.replace("\\s".toRegex(), "")
+                        .toInt() //strip potential whitespace and convert to int
                 chapters[current.c] = hashMapOf()
             }
             MARKER_VERSE_NUMBER -> {
@@ -77,17 +77,15 @@ class ParseUsfm(val file: File) {
                     //addFormattingToNextVerse(line)
                 } else {
                     // add this to the last verse
-                    addFormattingToLastVerse(line)
+                    addFormattingToVerse(line, current)
                 }
             }
         }
-
-
     }
 
-    private fun addFormattingToLastVerse(line: String) {
-        if (chapters.containsKey(current.c) && chapters[current.c]!!.containsKey(current.v)) {
-            chapters[current.c]!![current.v]!!.text += "$sep $line"
+    private fun addFormattingToVerse(line: String, verse: Current) {
+        if (chapters.containsKey(verse.c) && chapters[verse.c]!!.containsKey(verse.v)) {
+            chapters[verse.c]!![verse.v]!!.text += "$sep $line"
         }
     }
 }

src/main/kotlin/org/wycliffeassociates/otter/common/domain/resourcecontainer/ImportResourceContainer.kt
@@ -3,14 +3,11 @@ package org.wycliffeassociates.otter.common.domain.resourcecontainer
 import io.reactivex.Single
 import io.reactivex.schedulers.Schedulers
 import org.wycliffeassociates.otter.common.collections.tree.Tree
-import org.wycliffeassociates.otter.common.collections.tree.TreeNode
 import org.wycliffeassociates.otter.common.data.model.Collection
-import org.wycliffeassociates.otter.common.data.model.Content
-import org.wycliffeassociates.otter.common.domain.usfm.ParseUsfm
+import org.wycliffeassociates.otter.common.domain.resourcecontainer.project.IProjectReader
 import org.wycliffeassociates.otter.common.persistence.IDirectoryProvider
 import org.wycliffeassociates.otter.common.persistence.repositories.ICollectionRepository
 import org.wycliffeassociates.resourcecontainer.ResourceContainer
-import org.wycliffeassociates.resourcecontainer.entity.Project
 import java.io.File
 import java.io.IOException
 
@@ -18,20 +15,10 @@ class ImportResourceContainer(
         private val collectionRepository: ICollectionRepository,
         private val directoryProvider: IDirectoryProvider
 ) {
-    enum class Result {
-        SUCCESS,
-        INVALID_RC,
-        INVALID_CONTENT,
-        UNSUPPORTED_CONTENT,
-        IMPORT_ERROR,
-        LOAD_RC_ERROR,
-        ALREADY_EXISTS
-    }
-
-    fun import(file: File): Single<Result> {
+    fun import(file: File): Single<ImportResult> {
         return when {
             file.isDirectory -> importContainerDirectory(file)
-            else -> Single.just(Result.INVALID_RC)
+            else -> Single.just(ImportResult.INVALID_RC)
         }
     }
 
@@ -40,20 +27,20 @@ class ImportResourceContainer(
                 .just(directory)
                 .flatMap { containerDir ->
                     // Is this a valid resource container
-                    if (!validateResourceContainer(containerDir)) return@flatMap Single.just(Result.INVALID_RC)
+                    if (!validateResourceContainer(containerDir)) return@flatMap Single.just(ImportResult.INVALID_RC)
 
                     // Load the external container to get the metadata we need to figure out where to copy to
                     val extContainer = try {
                         ResourceContainer.load(containerDir, OtterResourceContainerConfig())
                     } catch (e: Exception) {
                         // Could be checked or unchecked exception from RC library
-                        return@flatMap Single.just(Result.LOAD_RC_ERROR)
+                        return@flatMap Single.just(ImportResult.LOAD_RC_ERROR)
                     }
                     val internalDir = directoryProvider.getSourceContainerDirectory(extContainer)
                     if (internalDir.exists() && internalDir.listFiles().isNotEmpty()) {
                         // Collision on disk: Can't import the resource container
                         // Assumes that filesystem internal app directory and database are in sync
-                        return@flatMap Single.just(Result.ALREADY_EXISTS)
+                        return@flatMap Single.just(ImportResult.ALREADY_EXISTS)
                     }
 
                     // Copy to the internal directory
@@ -63,20 +50,20 @@ class ImportResourceContainer(
                     val container = try {
                          ResourceContainer.load(newDirectory, OtterResourceContainerConfig())
                     } catch (e: Exception) {
-                        return@flatMap cleanUp(newDirectory, Result.LOAD_RC_ERROR)
+                        return@flatMap cleanUp(newDirectory, ImportResult.LOAD_RC_ERROR)
                     }
 
                     val (constructResult, tree) = constructContainerTree(container)
-                    if (constructResult != Result.SUCCESS) return@flatMap cleanUp(newDirectory, constructResult)
+                    if (constructResult != ImportResult.SUCCESS) return@flatMap cleanUp(newDirectory, constructResult)
 
                     return@flatMap collectionRepository
                             .importResourceContainer(container, tree, container.manifest.dublinCore.language.identifier)
-                            .toSingle { Result.SUCCESS }
+                            .toSingle { ImportResult.SUCCESS }
                             .doOnError { newDirectory.deleteRecursively() }
                 }
                 .subscribeOn(Schedulers.io())
 
-    private fun cleanUp(containerDir: File, result: Result): Single<Result> = Single.fromCallable {
+    private fun cleanUp(containerDir: File, result: ImportResult): Single<ImportResult> = Single.fromCallable {
         containerDir.deleteRecursively()
         return@fromCallable result
     }
@@ -95,15 +82,17 @@ class ImportResourceContainer(
         return destinationDirectory
     }
 
-    private fun makeExpandedContainer(container: ResourceContainer): Result {
+    private fun makeExpandedContainer(container: ResourceContainer): ImportResult {
         val dublinCore = container.manifest.dublinCore
         if (dublinCore.type == "bundle" && dublinCore.format.startsWith("text/usfm")) {
-            return if (container.expandUSFMBundle()) Result.SUCCESS else Result.INVALID_CONTENT
+            return if (container.expandUSFMBundle()) ImportResult.SUCCESS else ImportResult.INVALID_CONTENT
         }
-        return Result.SUCCESS
+        return ImportResult.SUCCESS
     }
 
-    private fun constructContainerTree(container: ResourceContainer): Pair<Result, Tree> {
+    private fun constructContainerTree(container: ResourceContainer): Pair<ImportResult, Tree> {
+        val projectReader = IProjectReader.build(container.manifest.dublinCore.format)
+                ?: return Pair(ImportResult.UNSUPPORTED_CONTENT, Tree(Unit))
         val root = Tree(container.toCollection())
         val categoryInfo = container.otterConfigCategories()
         for (project in container.manifest.projects) {
@@ -125,91 +114,13 @@ class ImportResourceContainer(
                     categoryNode
                 }
             }
-            val projectResult = constructProjectTree(container.dir, project)
-            if (projectResult.first == Result.SUCCESS) {
+            val projectResult = projectReader.constructProjectTree(container, project)
+            if (projectResult.first == ImportResult.SUCCESS) {
                 parent.addChild(projectResult.second)
             } else {
                 return Pair(projectResult.first, Tree(Unit))
             }
         }
-        return Pair(Result.SUCCESS, root)
-    }
-
-    private fun constructProjectTree(containerDir: File, project: Project): Pair<Result, Tree> {
-        var result = Result.SUCCESS
-        val projectLocation = containerDir.resolve(project.path)
-        val projectTree = Tree(project.toCollection())
-        if (projectLocation.isDirectory) {
-            val files = projectLocation.listFiles()
-            for (file in files) {
-                result = parseFileIntoProjectTree(file, projectTree, project.identifier)
-                if (result != Result.SUCCESS) return Pair(result, Tree(Unit))
-            }
-        } else {
-            // Single file
-            result = parseFileIntoProjectTree(projectLocation, projectTree, project.identifier)
-            if (result != Result.SUCCESS) return Pair(result, Tree(Unit))
-        }
-        return Pair(result, projectTree)
-    }
-
-    private fun parseFileIntoProjectTree(file: File, root: Tree, projectIdentifier: String): Result {
-        return when (file.extension) {
-            "usfm", "USFM" -> {
-                try {
-                    val chapters = parseUSFMToChapterTrees(file, projectIdentifier)
-                    root.addAll(chapters)
-                    Result.SUCCESS
-                } catch (e: RuntimeException) {
-                    Result.INVALID_CONTENT
-                }
-            }
-            else -> { Result.UNSUPPORTED_CONTENT }
-        }
-    }
-
-    private fun parseUSFMToChapterTrees(usfmFile: File, projectSlug: String): List<Tree> {
-        if (usfmFile.extension != "usfm") {
-            throw IOException("Not a USFM file")
-        }
-
-        val doc = ParseUsfm(usfmFile).parse()
-        return doc.chapters.map { chapter ->
-            val chapterSlug = "${projectSlug}_${chapter.key}"
-            val chapterCollection = Collection(
-                    chapter.key,
-                    chapterSlug,
-                    "chapter",
-                    chapter.key.toString(),
-                    null
-            )
-            val chapterTree = Tree(chapterCollection)
-            // create a chunk for the whole chapter
-            val chapChunk = Content(
-                    0,
-                    "chapter",
-                    chapter.value.values.first().number,
-                    chapter.value.values.last().number,
-                    null,
-                    null,
-                    null
-            )
-            chapterTree.addChild(TreeNode(chapChunk))
-
-            // Create content for each verse
-            for (verse in chapter.value.values) {
-                val content = Content(
-                        verse.number,
-                        "verse",
-                        verse.number,
-                        verse.number,
-                        null,
-                        null,
-                        null
-                )
-                chapterTree.addChild(TreeNode(content))
-            }
-            return@map chapterTree
-        }
+        return Pair(ImportResult.SUCCESS, root)
     }
 }

src/main/kotlin/org/wycliffeassociates/otter/common/domain/resourcecontainer/ImportResult.kt
@@ -1,0 +1,11 @@
+package org.wycliffeassociates.otter.common.domain.resourcecontainer
+
+enum class ImportResult {
+    SUCCESS,
+    INVALID_RC,
+    INVALID_CONTENT,
+    UNSUPPORTED_CONTENT,
+    IMPORT_ERROR,
+    LOAD_RC_ERROR,
+    ALREADY_EXISTS
+}

src/main/kotlin/org/wycliffeassociates/otter/common/domain/resourcecontainer/ResourceContainerExtensions.kt
@@ -1,7 +1,7 @@
 package org.wycliffeassociates.otter.common.domain.resourcecontainer
 
 import org.wycliffeassociates.otter.common.data.model.Collection
-import org.wycliffeassociates.otter.common.domain.usfm.ParseUsfm
+import org.wycliffeassociates.otter.common.domain.resourcecontainer.project.usfm.ParseUsfm
 import org.wycliffeassociates.resourcecontainer.ResourceContainer
 import org.wycliffeassociates.resourcecontainer.entity.Project
 import java.io.File

src/main/kotlin/org/wycliffeassociates/otter/common/domain/resourcecontainer/project/IProjectReader.kt
@@ -1,0 +1,20 @@
+package org.wycliffeassociates.otter.common.domain.resourcecontainer.project
+
+import org.wycliffeassociates.otter.common.collections.tree.Tree
+import org.wycliffeassociates.otter.common.domain.resourcecontainer.ImportResult
+import org.wycliffeassociates.otter.common.domain.resourcecontainer.project.markdown.MarkdownProjectReader
+import org.wycliffeassociates.otter.common.domain.resourcecontainer.project.usfm.UsfmProjectReader
+import org.wycliffeassociates.resourcecontainer.ResourceContainer
+import org.wycliffeassociates.resourcecontainer.entity.Project
+
+interface IProjectReader {
+    fun constructProjectTree(container: ResourceContainer, project: Project): Pair<ImportResult, Tree>
+
+    companion object {
+        fun build(format: String): IProjectReader? = when(format.toLowerCase()) {
+            "text/usfm" -> UsfmProjectReader()
+            "text/markdown" -> MarkdownProjectReader()
+            else -> null
+        }
+    }
+}

src/main/kotlin/org/wycliffeassociates/otter/common/domain/resourcecontainer/project/markdown/MarkdownProjectReader.kt
@@ -1,0 +1,13 @@
+package org.wycliffeassociates.otter.common.domain.resourcecontainer.project.markdown
+
+import org.wycliffeassociates.otter.common.collections.tree.Tree
+import org.wycliffeassociates.otter.common.domain.resourcecontainer.ImportResult
+import org.wycliffeassociates.otter.common.domain.resourcecontainer.project.IProjectReader
+import org.wycliffeassociates.resourcecontainer.ResourceContainer
+import org.wycliffeassociates.resourcecontainer.entity.Project
+
+class MarkdownProjectReader: IProjectReader {
+    override fun constructProjectTree(container: ResourceContainer, project: Project): Pair<ImportResult, Tree> {
+        TODO("The MarkdownProjectReader isn't yet implemented.")
+    }
+}

src/main/kotlin/org/wycliffeassociates/otter/common/domain/resourcecontainer/project/markdown/ParseMd.kt
@@ -1,4 +1,4 @@
-package org.wycliffeassociates.otter.common.domain.md
+package org.wycliffeassociates.otter.common.domain.resourcecontainer.project.markdown
 
 import java.io.BufferedReader
 

src/main/kotlin/org/wycliffeassociates/otter/common/domain/resourcecontainer/project/usfm/UsfmProjectReader.kt
@@ -1,0 +1,97 @@
+package org.wycliffeassociates.otter.common.domain.resourcecontainer.project.usfm
+
+import org.wycliffeassociates.otter.common.collections.tree.Tree
+import org.wycliffeassociates.otter.common.collections.tree.TreeNode
+import org.wycliffeassociates.otter.common.data.model.Collection
+import org.wycliffeassociates.otter.common.data.model.Content
+import org.wycliffeassociates.otter.common.domain.resourcecontainer.ImportResult
+import org.wycliffeassociates.otter.common.domain.resourcecontainer.project.IProjectReader
+import org.wycliffeassociates.otter.common.domain.resourcecontainer.toCollection
+import org.wycliffeassociates.resourcecontainer.ResourceContainer
+import org.wycliffeassociates.resourcecontainer.entity.Project
+import java.io.File
+import java.io.IOException
+
+class UsfmProjectReader: IProjectReader {
+    override fun constructProjectTree(
+            container: ResourceContainer, project: Project
+    ): Pair<ImportResult, Tree> {
+        var result = ImportResult.SUCCESS
+        val projectLocation = container.dir.resolve(project.path)
+        val projectTree = Tree(project.toCollection())
+        if (projectLocation.isDirectory) {
+            val files = projectLocation.listFiles()
+            for (file in files) {
+                result = parseFileIntoProjectTree(file, projectTree, project.identifier)
+                if (result != ImportResult.SUCCESS) return Pair(result, Tree(Unit))
+            }
+        } else {
+            // Single file
+            result = parseFileIntoProjectTree(projectLocation, projectTree, project.identifier)
+            if (result != ImportResult.SUCCESS) return Pair(result, Tree(Unit))
+        }
+        return Pair(result, projectTree)
+    }
+
+    private fun parseFileIntoProjectTree(file: File, root: Tree, projectIdentifier: String): ImportResult {
+        return when (file.extension) {
+            "usfm", "USFM" -> {
+                try {
+                    val chapters = parseUSFMToChapterTrees(file, projectIdentifier)
+                    root.addAll(chapters)
+                    ImportResult.SUCCESS
+                } catch (e: RuntimeException) {
+                    ImportResult.INVALID_CONTENT
+                }
+            }
+            else -> {
+                ImportResult.UNSUPPORTED_CONTENT
+            }
+        }
+    }
+
+    private fun parseUSFMToChapterTrees(usfmFile: File, projectSlug: String): List<Tree> {
+        if (usfmFile.extension != "usfm") {
+            throw IOException("Not a USFM file")
+        }
+
+        val doc = ParseUsfm(usfmFile).parse()
+        return doc.chapters.map { chapter ->
+            val chapterSlug = "${projectSlug}_${chapter.key}"
+            val chapterCollection = Collection(
+                    chapter.key,
+                    chapterSlug,
+                    "chapter",
+                    chapter.key.toString(),
+                    null
+            )
+            val chapterTree = Tree(chapterCollection)
+            // create a chunk for the whole chapter
+            val chapChunk = Content(
+                    0,
+                    "chapter",
+                    chapter.value.values.first().number,
+                    chapter.value.values.last().number,
+                    null,
+                    null,
+                    null
+            )
+            chapterTree.addChild(TreeNode(chapChunk))
+
+            // Create content for each verse
+            for (verse in chapter.value.values) {
+                val content = Content(
+                        verse.number,
+                        "verse",
+                        verse.number,
+                        verse.number,
+                        null,
+                        null,
+                        null
+                )
+                chapterTree.addChild(TreeNode(content))
+            }
+            return@map chapterTree
+        }
+    }
+}

WycliffeAssociates/otter-common

commit 4c89f9223e3e4eeea110a2910339052ca999fa9e

Fix bug introduced in #82.
The bug is ParseUsfm doesn't advance its chapter counter, causing imports to fail.

src/main/kotlin/org/wycliffeassociates/otter/common/domain/resourcecontainer/project/usfm/ParseUsfm.kt
@@ -25,21 +25,20 @@ class ParseUsfm(val file: File) {
     val chapters: UsfmDocument = UsfmDocument()
 
     fun parse(): ParseUsfm {
-        val reader = file.bufferedReader()
-        reader.use {
+        val fileCursor = Current()
+        file.bufferedReader().use {
             it.forEachLine {
-                parseLine(it)
+                parseLine(it, fileCursor)
             }
         }
         return this
     }
 
-    private fun parseLine(line: String) {
+    private fun parseLine(line: String, current: Current) {
         val split = line.split("\\s+".toRegex(), 2)
         if (split.isEmpty()) {
             return
         }
-        val current = Current()
         when (split[0]) {
             MARKER_BOOK_NAME -> return
             MARKER_CHAPTER_NUMBER -> {