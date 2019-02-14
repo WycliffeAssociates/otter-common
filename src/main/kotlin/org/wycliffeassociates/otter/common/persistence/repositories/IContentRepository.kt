6e33c754a4f81b37b9a0edf2872068cb1d5c638b
Kevin Joslyn <KJoslyn29@gmail.com>
2019-02-14 17:15:15 -0500

    Removed unused import

--- src/main/kotlin/org/wycliffeassociates/otter/common/persistence/repositories/IContentRepository.kt
+++ src/main/kotlin/org/wycliffeassociates/otter/common/persistence/repositories/IContentRepository.kt
@@ -1,7 +1,6 @@
 package org.wycliffeassociates.otter.common.persistence.repositories
 
 import io.reactivex.Completable
-import io.reactivex.Maybe
 import io.reactivex.Single
 import org.wycliffeassociates.otter.common.data.model.Content
 import org.wycliffeassociates.otter.common.data.model.Collection
