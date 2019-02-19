50b0a536ab7621620831c442ca3409d9eecc0f9b
Kevin Joslyn <KJoslyn29@gmail.com>
2019-02-19 17:32:22 -0500

    Creation of OtterTree from zip file works, several issues remain
    1. Collection slugs are incorrect
    2. chapters, verses, and body/notes are being put into the tree backwards

--- src/main/kotlin/org/wycliffeassociates/otter/common/domain/resourcecontainer/project/OtterZipFile.kt
+++ src/main/kotlin/org/wycliffeassociates/otter/common/domain/resourcecontainer/project/OtterZipFile.kt
@@ -1,21 +1,33 @@
 package org.wycliffeassociates.otter.common.domain.resourcecontainer.project
 
 import java.io.BufferedReader
-import java.io.File
 import java.util.zip.ZipEntry
 import java.util.zip.ZipFile
 
 class OtterZipFile(
         val absolutePath: String,
         private val rootZipFile: ZipFile,
-        val parentFile: OtterZipFile? = null,
+        separator: String,
+        val parentFile: OtterFile? = null,
         private val zipEntry: ZipEntry? = null
 ) {
     val isFile = zipEntry != null
-    val nameWithoutExtension = absolutePath.split(File.separator, ".").last() // TODO: Unit test
-    val name: String = absolutePath.substring(absolutePath.lastIndexOf(File.separator) + 1) // TODO: Unit test. We aren't using prefix length
+    val nameWithoutExtension = absolutePath.split(separator, ".").dropLast(1).last() // TODO: Unit test or take the name between a possible slash and the last period
+    val name: String = absolutePath.substring(absolutePath.lastIndexOf(separator) + 1) // TODO: Unit test. We aren't using prefix length
 
     fun bufferedReader(): BufferedReader = rootZipFile.getInputStream(zipEntry).bufferedReader()
     fun toRelativeString(parent: OtterFile): String = absolutePath.substringAfter(parent.absolutePath) // TODO: Unit test
+
+    companion object {
+        fun otterFileZ(
+                absolutePath: String,
+                rootZipFile: ZipFile,
+                separator: String,
+                parentFile: OtterFile? = null,
+                zipEntry: ZipEntry? = null
+        ): OtterFile {
+            return OtterFile.Z(OtterZipFile(absolutePath, rootZipFile, separator, parentFile, zipEntry))
+        }
+    }
 }
 
