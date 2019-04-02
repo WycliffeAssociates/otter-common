a5ff405e487bfbd77e3f131dd2aa3934c1e3c243
Russell Aunger <rba@live.com>
2019-04-02 16:26:19 -0400

    Take model object += createdTimestamp, deletedTimestamp.

--- src/main/kotlin/org/wycliffeassociates/otter/common/data/rxmodel/Take.kt
+++ src/main/kotlin/org/wycliffeassociates/otter/common/data/rxmodel/Take.kt
@@ -1,12 +1,14 @@
 package org.wycliffeassociates.otter.common.data.rxmodel
 
-import io.reactivex.subjects.BehaviorSubject
+import com.jakewharton.rxrelay2.BehaviorRelay
 import java.io.File
+import java.time.LocalDate
 
 data class Take(
         val name: String,
         val file: File,
         val number: Int,
         val format: MimeType,
-        val deleted: BehaviorSubject<Boolean>
+        val createdTimestamp: LocalDate,
+        val deletedTimestamp: BehaviorRelay<Long?>
 )
