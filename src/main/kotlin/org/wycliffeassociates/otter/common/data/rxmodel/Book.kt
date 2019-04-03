d9f3453f43e6ce38dbf687115f3367641e149028
Russell Aunger <rba@live.com>
2019-04-03 15:20:38 -0400

    Remove DB id fields from Chapter&Chunk bc we're totally rxadical now.

--- src/main/kotlin/org/wycliffeassociates/otter/common/data/rxmodel/Book.kt
+++ src/main/kotlin/org/wycliffeassociates/otter/common/data/rxmodel/Book.kt
@@ -3,9 +3,8 @@ package org.wycliffeassociates.otter.common.data.rxmodel
 import io.reactivex.Observable
 
 data class Book(
+    val sort: Int,
     val title: String,
     val chapters: Observable<Chapter>,
-    val hasResources: Boolean,
-    val sort: Int,
-    val id: Int
+    val hasResources: Boolean
 )
--- src/main/kotlin/org/wycliffeassociates/otter/common/data/rxmodel/Resource.kt
+++ src/main/kotlin/org/wycliffeassociates/otter/common/data/rxmodel/Resource.kt
@@ -1,7 +1,6 @@
 package org.wycliffeassociates.otter.common.data.rxmodel
 
 data class Resource(
-    val id: Int,
     val sort: Int,
     val title: TextItem,
     val body: TextItem?,
