7d29349b0645d40c6f61f72dd30659df4366587e
Russell Aunger <rba@live.com>
2019-04-29 03:26:55 -0400

    Update IResourceRepository for ResourceGroup impl.

--- src/main/kotlin/org/wycliffeassociates/otter/common/persistence/repositories/IResourceRepository.kt
+++ src/main/kotlin/org/wycliffeassociates/otter/common/persistence/repositories/IResourceRepository.kt
@@ -1,18 +1,22 @@
 package org.wycliffeassociates.otter.common.persistence.repositories
 
 import io.reactivex.Completable
+import io.reactivex.Observable
 import io.reactivex.Single
 import org.wycliffeassociates.otter.common.data.model.Content
 import org.wycliffeassociates.otter.common.data.model.Collection
+import org.wycliffeassociates.otter.common.data.workbook.HasChildBookElements
+import org.wycliffeassociates.otter.common.data.workbook.ResourceContainerInfo
 
 interface IResourceRepository : IRepository<Content> {
-    // Get resources for a content
-    fun getByContent(content: Content): Single<List<Content>>
-    // Get resources for a collection
-    fun getByCollection(collection: Collection): Single<List<Content>>
-    // Link
+    fun getResources(collection: Collection, rc: ResourceContainerInfo): Observable<Content>
+    fun getResources(content: Content, rc: ResourceContainerInfo): Observable<Content>
+    fun getSubtreeResourceInfo(collection: Collection): List<ResourceContainerInfo>
+    fun getResourceContainerInfo(content: Content): List<ResourceContainerInfo>
+    fun getResourceContainerInfo(collection: Collection): List<ResourceContainerInfo>
     fun linkToContent(resource: Content, content: Content, dublinCoreFk: Int): Completable
     fun linkToCollection(resource: Content, collection: Collection, dublinCoreFk: Int): Completable
+
     // Prepare SubtreeHasResources table
     fun calculateAndSetSubtreeHasResources(collectionId: Int)
 }
