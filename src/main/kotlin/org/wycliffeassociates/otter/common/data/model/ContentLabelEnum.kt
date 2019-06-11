a3cfff7afec4fc823fb3cc61c95f544f65cdceb3
Russell Aunger <rba@live.com>
2019-06-11 14:41:15 -0400

    Extract ContentLabelEnum to common.data.model.

---
+++ src/main/kotlin/org/wycliffeassociates/otter/common/data/model/ContentLabelEnum.kt
@@ -0,0 +1,8 @@
+package org.wycliffeassociates.otter.common.data.model
+
+enum class ContentLabelEnum(val value: String) {
+    CHAPTER("chapter"),
+    VERSE("verse"),
+    HELP_TITLE("title"),
+    HELP_BODY("body")
+}
