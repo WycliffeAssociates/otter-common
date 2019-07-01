d8af9263f6338aaf2d4c03a6da19b8bf46aef10b
Joe <joe_sarabia@wycliffeassociates.org>
2019-07-01 11:15:48 -0400

    port of tR recorder

---
+++ src/main/kotlin/org/wycliffeassociates/otter/common/collections/FloatRingBuffer.kt
@@ -0,0 +1,62 @@
+package org.wycliffeassociates.otter.common.collections
+
+
+/**
+ * Created by sarabiaj on 2/21/2017.
+ */
+class FloatRingBuffer(capacity: Int) {
+
+    private var head = 0
+    private var tail = 0
+    private val buffer = FloatArray(capacity)
+    private val returnBuffer = FloatArray(capacity * 4)
+
+    val isEmpty: Boolean
+        @Synchronized
+        get() = head == tail
+
+    val array: FloatArray
+        @Synchronized
+        get() {
+            var i = 0
+            val length = size()
+            while (i < length) {
+                returnBuffer[i * 4] = i.toFloat()
+                returnBuffer[i * 4 + 1] = get(i)
+                returnBuffer[i * 4 + 2] = i.toFloat()
+                returnBuffer[i * 4 + 3] = get(i + 1)
+                i += 2
+            }
+            return returnBuffer
+        }
+
+    @Synchronized
+    fun add(i: Float) {
+        buffer[tail] = i
+        tail = (tail + 1) % buffer.size
+        if (head == tail) {
+            head = (head + 1) % buffer.size
+        }
+    }
+
+    @Synchronized
+    fun clear() {
+        head = 0
+        tail = 0
+    }
+
+    @Synchronized
+    operator fun get(i: Int): Float {
+        val index = (head + i) % buffer.size
+        return buffer[index]
+    }
+
+    @Synchronized
+    fun size(): Int {
+        return if (head == 0 && tail < buffer.size) {
+            tail
+        } else {
+            buffer.size
+        }
+    }
+}
---
+++ src/main/kotlin/org/wycliffeassociates/otter/common/recorder/WavFileWriter.kt
@@ -0,0 +1,30 @@
+package org.wycliffeassociates.otter.common.app
+
+import io.reactivex.Observable
+import io.reactivex.schedulers.Schedulers
+import org.wycliffeassociates.otter.common.io.wav.WavFile
+import org.wycliffeassociates.otter.common.io.wav.WavOutputStream
+
+class WavFileWriter(
+    private val wav: WavFile,
+    private val audioStream: Observable<ByteArray>,
+    private val onComplete: () -> Unit
+)
+{
+    val writer = Observable.using(
+        {
+            WavOutputStream(wav, false, WavOutputStream.BUFFERING.BUFFERED)
+        },
+        {
+            writer -> audioStream.map {
+                writer.write(it)
+            }
+        },
+        {
+            writer ->
+                writer.close()
+                onComplete()
+        }
+    ).subscribeOn(Schedulers.io())
+    .subscribe()
+}
